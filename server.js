5f332dc0e9c8227dbe0d7d7547b94011374a5b03
Ravi Shah <Ravi_Shah01@ad.infosys.com>
2017-04-22 23:32:54 +0530

    ''

--- server.js
+++ server.js
@@ -1,40 +1,41 @@
-var nodemailer = require('nodemailer');
+//var nodemailer = require('nodemailer');
 var express = require('express');
 var app = express();
 
 app.post('/', function(req, res){
 
     // Not the movie transporter!
-    var transporter = nodemailer.createTransport({
-        service: 'Gmail',
-		port:'587',
-		secure:false,
-		ignoreTLS:true,
-        auth: {
-            user: 'ambarish161@gmail.com', // Your email id
-            pass: 'ibm@2014' // Your password
-        }
-    });
+//    var transporter = nodemailer.createTransport({
+//        service: 'Gmail',
+//		port:'587',
+//		secure:false,
+//		ignoreTLS:true,
+ //       auth: {
+//            user: 'ambarish161@gmail.com', // Your email id
+//            pass: 'ibm@2014' // Your password
+//        }
+//    });
 	
-	var text = 'Hello world from \n\n' + 'Ravi';
+//	var text = 'Hello world from \n\n' + 'Ravi';
 	
-	var mailOptions = {
-		from: 'ambarish161@gmail.com', // sender address
-		to: 'shah.ravir@gmail.com', // list of receivers
-		subject: 'Email Example', // Subject line
-		text: text //, // plaintext body
+//	var mailOptions = {
+//		from: 'ambarish161@gmail.com', // sender address
+//		to: 'shah.ravir@gmail.com', // list of receivers
+//		subject: 'Email Example', // Subject line
+//		text: text //, // plaintext body
 		// html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
-	};
+//	};
 	
-	transporter.sendMail(mailOptions, function(error, info){
-		if(error){
-			console.log(error);
-			res.json({yo: 'error'});
-		}else{
-			console.log('Message sent: ' + info.response);
-			res.json({yo: info.response});
-		};
-});
+//	transporter.sendMail(mailOptions, function(error, info){
+//		if(error){
+//			console.log(error);
+//			res.json({yo: 'error'});
+//		}else{
+//			console.log('Message sent: ' + info.response);
+//			res.json({yo: info.response});
+//		};
+//});
+	res.json("TESTING")
 });
 
 
